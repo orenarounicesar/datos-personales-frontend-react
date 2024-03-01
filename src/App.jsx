0201d25818d208d0d2725b789172f1e9551732d5
CarlosArroyo21 <carlosdanielarroyocardona11@gmail.com>
2024-02-29 20:01:16 -0500

    theme establecido y router inicializado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Fragment, useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import Home from './components/Home'
+import List from './components/List'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
           <BrowserRouter>
           <Routes>
             <Route path="/*" element={<Home/>} >
+              <Route path='list' element={<List/>} />
               </Route>
 
           </Routes>
